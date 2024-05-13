567cb955dff9f57d7daf5a4bdb4305d216f919fe
Carl Cortright <ckcortright@gmail.com>
2024-05-12 20:58:00 -0400

    fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="flyflowclient",
-    version="1.14.4",
+    version="1.14.6",
     packages=find_packages(),
     install_requires=[
         "requests>=2.25.1"
